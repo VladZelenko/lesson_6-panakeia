e5bd03b054f0369a47d2bf8c8c13b0d44506cc9d
VladZelenko <Vlad6555@ukr.net>
2017-12-01 16:35:52 +0200

    add section news+style

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,5 +1,4 @@
 $(".button-collapse").sideNav();
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-$(document).ready(function(){
-      $('.parallax').parallax();
-    });
+// $(document).ready(function(){
+//       $('.parallax').parallax();
+//     });
